bf1e28b7db6dacaec27d545646246cdd6902e944
Youness EL Lamzi <yellamzi@ctg.com>
2025-03-09 15:50:22 +0100

    Fix: Enable ESM for serverless functions

--- api/index.js
+++ api/index.js
@@ -4,12 +4,9 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const serverModulePath = path.join(
-  __dirname,
-  "../dist/al-buraq/server/server.mjs"
-);
-
 export default async (req, res) => {
-  const { app } = await import(serverModulePath);
+  const { app } = await import(
+    path.join(__dirname, "../dist/al-buraq/server/server.mjs")
+  );
   return app(req, res);
 };
